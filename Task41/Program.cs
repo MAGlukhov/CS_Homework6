13c80e807ecff4d0132054fabf0cc5ca7b467356
Maxim <Thickfod@gmail.com>
2022-09-14 23:17:20 +0300

    Homework 6

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+
+int ZerosCounter(string word)
+{
+    int result = 0;
+    for(int i = 0; i < word.Length; i++)
+    {
+        if(word[i] == '0')
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+Console.WriteLine(ZerosCounter(number));
